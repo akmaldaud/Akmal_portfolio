f70f044bb8ae1e30cfc737035efb7f9c8527bd0d
Akmal <56720171+akmaldaud@users.noreply.github.com>
2025-08-30 15:15:53 +0800

    Add files via upload

---
+++ Rust/rust_variable/target/debug/deps/rust_variable.d
@@ -0,0 +1,5 @@
+D:\Rust\rust_variable\target\debug\deps\rust_variable.d: src\main.rs
+
+D:\Rust\rust_variable\target\debug\deps\rust_variable.exe: src\main.rs
+
+src\main.rs:
---
+++ Rust/rust_variable/target/debug/rust_variable.d
@@ -0,0 +1,1 @@
+D:\Rust\rust_variable\target\debug\rust_variable.exe: D:\Rust\rust_variable\src\main.rs
