0d9dd308996aa35ec62f0e2d54cf01c15638376e
Akmal <56720171+akmaldaud@users.noreply.github.com>
2025-09-03 23:04:22 +0800

    Create lst_comprehnsions.py

---
+++ Python/lst_comprehnsions.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    output=[]
+    ijk=[]
+    for X in range(x+1):
+        for Y in range(y+1):
+            for Z in range(z+1):
+                if X+Y+Z!=n:
+                    ijk=[X,Y,Z];
+                    output.append(ijk);
+    print(output)
