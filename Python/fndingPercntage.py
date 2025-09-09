a5220a1759ee4f17a345679a2607ed8f2e722340
Akmal <56720171+akmaldaud@users.noreply.github.com>
2025-09-09 15:51:48 +0800

    Create fndingPercntage.py
    The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. Print the average of the marks array for the student name provided, showing 2 places after the decimal.
    
    https://www.hackerrank.com/challenges/finding-the-percentage/problem?isFullScreen=true

---
+++ Python/fndingPercntage.py
@@ -0,0 +1,17 @@
+def average_marks(student_marks, query_name):
+    marks = student_marks[query_name]
+    average = sum(marks) / len(marks)
+    print("{:.2f}".format(average))
+
+def main():
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    average_marks(student_marks, query_name)
+
+if __name__ == '__main__':
+    main()
